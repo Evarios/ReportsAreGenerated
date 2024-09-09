382dd2d29bcc2fe04ab66a413908c06f200e71f2
lucasosiewicz <osiewicz87@gmail.com>
2024-09-09 08:26:44 +0200

    Preprocessing metadata.sql

--- README.md
+++
@@ -1,2 +0,0 @@
-# ReportsAreGenerated
-Goal of this project is to build RAG which is capable to create plots based on database's data
--- prompt.py
+++
@@ -1,9 +0,0 @@
-prompt = """
-Write a query in for postgreSQL which will handle the question.
-Return only the SQL code.
-Constraints:
-{constraints}
-Tables and columns:
-{tables}
-Question: {query}
-"""
